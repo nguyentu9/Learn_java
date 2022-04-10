1096c02889b5a8c05b3c505b832a560c17281fb3
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 18:30:19 +0700

    45 Extracting Methods

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,12 +1,62 @@
 package com.example;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        String message = greetUser("Toan", "Huynh");
+
+        int principal = 0;
+        float annualInterest;
+        byte years = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000");
+        }
+
+        while (true) {
+            System.out.println("Annual Interest Rate: ");
+            annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+        while (true) {
+            System.out.println("Period (Years): ");
+            years = scanner.nextByte();
+            if (years >= 1 && years <= 30) {
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
     }
 
-    private static String greetUser(String firstName, String lastName) {
-        return "Hello " + firstName + " " + lastName;
+    public static double calculateMortgage(int principal, float annualInterest, byte years) {
+
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        short numberOfPayments = (short)(years * MONTHS_IN_YEAR);
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        return mortgage;
     }
 }
