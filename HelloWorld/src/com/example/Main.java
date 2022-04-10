db0efa4ea1f011dba8e819ee5831e05c58a49bb7
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 17:05:10 +0700

    34 While Loops

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,10 +1,16 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 1; i < 5; i++){
-            System.out.println("Hello World");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.println("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
         }
     }
 }
