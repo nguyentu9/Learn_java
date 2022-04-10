8817d753d8da703b7b9ecf7a910d9bd8abec4833
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 17:08:27 +0700

    36 Break and Continue Statements

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -7,16 +7,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.equals("quit")) {
+        while (true) {
             System.out.println("Input: ");
             input = scanner.next().toLowerCase();
+            if(input.equals("pass"))
+                continue;
+            if (input.equals("quit"))
+                break;
             System.out.println(input);
         }
-
-        do {
-            System.out.println("Input: ");
-            input = scanner.next().toLowerCase();
-            System.out.println(input);
-        } while(!input.equals("quit"));
     }
 }
