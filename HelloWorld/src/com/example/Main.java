f2c52eeb2400eef6995e28d9e36fe96d0ad328e0
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 15:26:32 +0700

    10 Reference Types

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,14 +1,13 @@
 package com.example;
 
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
         byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        double price = 10.99;
-        float priceF = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
+        Date now = new Date();
+        System.out.println(now);
 
     }
 }
