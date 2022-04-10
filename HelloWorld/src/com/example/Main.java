6340fbae302edaa2d72ee83ac39731395acc1fc7
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 16:38:28 +0700

    28 If Statements

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -3,10 +3,16 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        boolean hasHighIncome = false;
-        boolean hasGoodCredit = true;
-        boolean hasCriminalRecord = false;
-        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
-        System.out.println(isEligible);
+        int temp = 32;
+        if(temp > 30) {
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        }
+        else if(temp > 20) {
+            System.out.println("Beautiful day");
+        }
+        else {
+            System.out.println("Cold day");
+        }
     }
 }
