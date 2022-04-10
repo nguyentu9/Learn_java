6577c715d48e945c100a399b96add1343d2531b1
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 15:58:04 +0700

    16 Constants

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,17 +1,9 @@
 package com.example;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        int[][] numbers = new int[2][3];
-        numbers[0][0] = 1;
-        System.out.println(Arrays.toString(numbers)); // [[I@7c30a502, [I@49e4cb85]
-        System.out.println(Arrays.deepToString(numbers)); // [[1, 0, 0], [0, 0, 0]]
-
-        int[][] numbers2 = { {1,2,3}, {4,5,6} };
-        System.out.println(Arrays.deepToString(numbers2)); // [[1, 2, 3], [4, 5, 6]]
-
+        final float PI = 3.14F;
+        // PI = 1;  -> error
     }
 }
