828165169b1a8ab12310f64c76554913e9150cee
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 16:36:46 +0700

    27 Logical Operators

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -3,8 +3,10 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1;
-        int y = 2;
-        System.out.println(x <= y);
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+        System.out.println(isEligible);
     }
 }
