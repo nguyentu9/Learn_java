4b2c7845558c113b8bd08a23759d793a13cb10ea
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 15:52:47 +0700

    15 Multi dimensional Arrays

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -5,15 +5,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        // numbers[10] = 3; // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 5
-        System.out.println(Arrays.toString(numbers)); // [1, 2, 0, 0, 0]
+        int[][] numbers = new int[2][3];
+        numbers[0][0] = 1;
+        System.out.println(Arrays.toString(numbers)); // [[I@7c30a502, [I@49e4cb85]
+        System.out.println(Arrays.deepToString(numbers)); // [[1, 0, 0], [0, 0, 0]]
 
-        int[] numbers2 = {2, 3, 5, 1, 4};
-        Arrays.sort(numbers2);
-        System.out.println(numbers2.length); // 5
-        System.out.println(Arrays.toString(numbers2)); // [1, 2, 3, 4, 5]
+        int[][] numbers2 = { {1,2,3}, {4,5,6} };
+        System.out.println(Arrays.deepToString(numbers2)); // [[1, 2, 3], [4, 5, 6]]
+
     }
 }
