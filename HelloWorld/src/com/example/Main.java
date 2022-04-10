bf8943b149cdf8db6c6ba95d3cc62bdd6817a9a4
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 15:21:39 +0700

    9 Primitive Types

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -3,8 +3,12 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        double price = 10.99;
+        float priceF = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
     }
 }
