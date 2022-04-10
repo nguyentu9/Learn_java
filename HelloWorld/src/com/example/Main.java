30eb8463c04fce4c748193cee89cd1d16f507831
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 15:30:38 +0700

    11 Primitive vs Reference Types

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,13 +1,14 @@
 package com.example;
 
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
-
+        Point point1 = new Point(1 , 1);
+        Point point2 = point1;
+        point1.x = 2;
+        System.out.println(point2);
     }
 }
