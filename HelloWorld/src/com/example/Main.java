70c431af856e5e063aede9061164a41bda9fef55
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 16:16:27 +0700

    21 Reading Input

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,21 +1,13 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int result = Math.round(1.1F);
-        System.out.println(result); // 1
-
-        result = (int)Math.ceil(1.1F);
-        System.out.println(result); // 2
-
-        result = (int)Math.floor(1.1F);
-        System.out.println(result); // 1
-
-        result = Math.max(1, 2);
-        System.out.println(result); // 2
-
-        int result2 = (int)(Math.random() * 100);
-        System.out.println(result2);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String name = scanner.nextLine().trim();
+        System.out.println("You are: " + name);
     }
 }
