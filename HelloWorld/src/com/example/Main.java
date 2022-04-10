d622630c54d8405d5d34dba7aac7918a09734367
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 17:02:35 +0700

    32 Exercise FizzBuzz

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,18 +1,22 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int role = 1;
-        switch(role) {
-            case 1:
-                System.out.println("You're an admin");
-                break;
-            case 2:
-                System.out.println("You're a moderator");
-                break;
-            default:
-                System.out.println("You're a guest");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Number: ");
+        int number = scanner.nextInt();
+
+        if(number % 5 == 0 && number % 3 == 0){
+            System.out.println("FizzBuzz");
+        } else if(number % 5 == 0){
+            System.out.println("Fizz");
+        } else if(number % 3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
         }
     }
 }
