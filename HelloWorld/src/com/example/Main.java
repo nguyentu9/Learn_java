f3c808d953d8f0b081ff20f94cac9fcfe4f5d5ca
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 15:40:57 +0700

    13 Escape Sequences

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,18 +1,11 @@
 package com.example;
 
-import java.awt.*;
-import java.util.Locale;
-
 public class Main {
 
     public static void main(String[] args) {
-        String message = "  Hello World" + "!!  ";
-        System.out.println(message.endsWith("!!")); // true
-        System.out.println(message.length()); // 17
-        System.out.println(message.indexOf("e")); // 3
-        System.out.println(message.indexOf("sky")); // -1
-        System.out.println(message.replace("!", "*")); // Hello World**
-        System.out.println(message.toLowerCase()); // hello world!!
-        System.out.println(message.trim()); // Hello World!!
+        String message = "Hello \"Toan\"";
+        String message2 = "c:\\Windows\\..."; // c:\Windows\...
+        System.out.println(message);
+        System.out.println(message2);
     }
 }
