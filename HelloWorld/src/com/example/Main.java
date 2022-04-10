4cb3684eae6793adfb13374fa13bb4ae87a9e9b5
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 16:04:42 +0700

    19 Casting

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -3,10 +3,14 @@ package com.example;
 public class Main {
 
     public static void main(String[] args) {
-        // Order of Operatiors
-        // () -> */ -> +-
-        int x = (10 + 3) * 2;
-        System.out.println(x); // 26
+        // Implicit casting
+        // byte > long > int > long > float > double
+        String x = "1";
+        int y = Integer.parseInt(x) + 2;
+        System.out.println(y); // 3
 
+        String x2 = "1.1";
+        double y2 = Double.parseDouble(x2) + 2;
+        System.out.println(y2); // 3.1
     }
 }
