ccbda8e44ef6237d5bca99172f7f230502997f9f
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 15:46:41 +0700

    14  Arrays

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,11 +1,19 @@
 package com.example;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        String message = "Hello \"Toan\"";
-        String message2 = "c:\\Windows\\..."; // c:\Windows\...
-        System.out.println(message);
-        System.out.println(message2);
+        int[] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        // numbers[10] = 3; // Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 10 out of bounds for length 5
+        System.out.println(Arrays.toString(numbers)); // [1, 2, 0, 0, 0]
+
+        int[] numbers2 = {2, 3, 5, 1, 4};
+        Arrays.sort(numbers2);
+        System.out.println(numbers2.length); // 5
+        System.out.println(Arrays.toString(numbers2)); // [1, 2, 3, 4, 5]
     }
 }
