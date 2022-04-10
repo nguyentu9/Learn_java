b25f41a974ef5c90bd5bf483d907c3af52ec0b0b
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-10 18:20:58 +0700

    43 Creating Methods

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,51 +1,12 @@
 package com.example;
 
-import java.text.NumberFormat;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+        String message = greetUser("Toan", "Huynh");
+    }
 
-        int principal = 0;
-        float monthlyInterest = 0;
-        int numberOfPayments = 0;
-
-        Scanner scanner = new Scanner(System.in);
-        while(true) {
-            System.out.println("Principal: ");
-            principal = scanner.nextInt();
-            if(principal >= 1000 && principal <= 1_000_000)
-                break;
-            System.out.println("Enter a value between 1000 and 1000000");
-        }
-
-        while(true) {
-            System.out.println("Annual Interest Rate: ");
-            float annualInterst = scanner.nextFloat();
-            if(annualInterst >= 1 && annualInterst <= 30) {
-                monthlyInterest = annualInterst / PERCENT / MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30");
-        }
-
-        while(true) {
-            System.out.println("Period (Years): ");
-            byte years = scanner.nextByte();
-            if(years >= 1 && years <= 30) {
-                numberOfPayments = years * MONTHS_IN_YEAR;
-                break;
-            }
-            System.out.println("Enter a value between 1 and 30");
-        }
-
-        double mortgage = principal
-                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
-                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
-        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
+    private static String greetUser(String firstName, String lastName) {
+        return "Hello " + firstName + " " + lastName;
     }
 }
