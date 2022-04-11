30e7d1eedb686f0133c206a9ac3ab438f8f40b71
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-11 20:26:21 +0700

    128 The try with resources Statement

--- HelloWorld/src/com/example/exceptions/ExceptionsDemo.java
+++ HelloWorld/src/com/example/exceptions/ExceptionsDemo.java
@@ -2,6 +2,7 @@ package com.example.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -16,27 +17,14 @@ import java.text.SimpleDateFormat;
 */
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try(
+            var  reader = new FileReader("file.txt");
+            var writer = new FileWriter("...");
+        ) {
             var value = reader.read();
-            new SimpleDateFormat().parse("");
         }
-        catch (FileNotFoundException e) {
-            System.out.println(e.getMessage());
-            System.out.println("File does not exist.");
-        }
-        catch(IOException | ParseException e){
+        catch(IOException e){
             System.out.println("Cound not read data.");
-        }
-        finally {
-            if(reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
     public static void sayHello(String name) {
