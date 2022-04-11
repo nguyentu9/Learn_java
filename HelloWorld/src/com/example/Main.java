a6a2ffa3312d6193c75adcc356e4393e33b1885e
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-11 20:03:23 +0700

    122 What are Exceptions

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,14 +1,11 @@
 package com.example;
 
+import com.example.exceptions.ExceptionsDemo;
+
 public class Main {
 
     public static void main(String[] args) {
-        var box1 = new TextBox();
-        var box2 = box1;
-        System.out.println(box1.hashCode());
-        System.out.println(box2.hashCode());
-        System.out.println(box1.equals(box2));
-        System.out.println(box1.toString());
+        ExceptionsDemo.show();
     }
 
 }
