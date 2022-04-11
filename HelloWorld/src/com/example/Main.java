c968f944fdab3aac8ffae7056b753ef6e363dbf0
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-11 22:10:02 +0700

    138 Generics and Primitive Types

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -6,8 +6,12 @@ import com.example.generics.User;
 public class Main {
 
     public static void main(String[] args) {
-        var list = new GenericList<User>();
-        list.add(new User());
-        User number = list.get(0);
+        // int -> Integer
+        // float -> Float
+        // boolean -> Boolean
+
+        GenericList<Integer> numbers = new GenericList<>();
+        numbers.add(1); // Boxing
+        int number = numbers.get(0); // Unboxing;
     }
 }
