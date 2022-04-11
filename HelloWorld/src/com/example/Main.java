9f636c88fbcaefc52a037897c8b9da24fed1db6d
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-11 22:03:58 +0700

    136 A Poor Solution

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,13 +1,17 @@
 package com.example;
 
-import com.example.exceptions.ExceptionsDemo;
 import com.example.generics.List;
+import com.example.generics.User;
 
 public class Main {
 
     public static void main(String[] args) {
         var list = new List();
         list.add(1);
-    }
+        list.add(Integer.valueOf(1));
+        list.add("1");
+        list.add(new User());
 
+        int number = (int)list.get(1); // -> get error
+    }
 }
