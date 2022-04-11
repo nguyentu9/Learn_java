b497cfc8a3624839a8422a5380a2bd86b9321c04
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-11 22:07:57 +0700

    137 Generic Classes

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,17 +1,13 @@
 package com.example;
 
-import com.example.generics.List;
+import com.example.generics.GenericList;
 import com.example.generics.User;
 
 public class Main {
 
     public static void main(String[] args) {
-        var list = new List();
-        list.add(1);
-        list.add(Integer.valueOf(1));
-        list.add("1");
+        var list = new GenericList<User>();
         list.add(new User());
-
-        int number = (int)list.get(1); // -> get error
+        User number = list.get(0);
     }
 }
