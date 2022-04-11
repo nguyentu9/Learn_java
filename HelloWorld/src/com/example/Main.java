30379f70573f06bf11346906dc706a4d961bcc17
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-11 07:39:09 +0700

    74 Constructor Overloading

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
+        new Employee(10_000);
         var employee = new Employee(50, 20);
         int wage = employee.calculateWage();
 
