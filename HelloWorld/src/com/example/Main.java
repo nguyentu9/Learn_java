bfb923e86768e83b9b12e1049171e4a907c4db39
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-11 07:32:43 +0700

    73 Method Overloading

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         var employee = new Employee(50, 20);
-        int wage = employee.calculateWage(10);
+        int wage = employee.calculateWage();
 
         System.out.println(wage);
     }
