80b411ccafc038848c55bed957ee6ccc662e23c3
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-11 07:51:35 +0700

    75 Static Members

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -3,6 +3,7 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         new Employee(10_000);
+        System.out.println(Employee.numberOfEmployees);
         var employee = new Employee(50, 20);
         int wage = employee.calculateWage();
 
