edb0a5af19328982abb408e42108b6f8c6896305
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-12 15:11:44 +0700

    152 The Collection Interface

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,18 +1,15 @@
 package com.example;
 
+import com.example.collections.CollectionsDemo;
 import com.example.generics.GenericList;
 import com.example.generics.Instructor;
 import com.example.generics.User;
 import com.example.generics.Utils;
 
+
 public class Main {
 
     public static void main(String[] args) {
-        var list = new GenericList<String>();
-        list.add("a");
-        list.add("b");
-        for(var item:list)
-            System.out.println(item);
-
+        CollectionsDemo.show();
     }
 }
