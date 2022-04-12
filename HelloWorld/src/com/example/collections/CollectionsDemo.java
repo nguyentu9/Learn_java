8f51fa075eb64d5d72d31757fa94b48d5fec0395
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-12 15:30:20 +0700

    154 The Comparable Interface

--- HelloWorld/src/com/example/collections/CollectionsDemo.java
+++ HelloWorld/src/com/example/collections/CollectionsDemo.java
@@ -7,20 +7,12 @@ import java.util.List;
 
 public class CollectionsDemo {
     public static void show() {
-        List<String> list = new ArrayList<>();
-//        list.add("a");
-//        list.add("b");
-//        list.add("c");
-//        list.add(0, "!");
-//        System.out.println(list);
+        List<Customer> customers = new ArrayList<>();
+        customers.add(new Customer("b"));
+        customers.add(new Customer("a"));
+        customers.add(new Customer("c"));
+        Collections.sort(customers);
 
-        Collections.addAll(list, "a", "b", "c");
-        System.out.println(list);
-        list.set(0, "a+");
-        System.out.println(list);
-        System.out.println(list.get(0));
-
-        System.out.println(list.subList(0, 2));
-
+        System.out.println(customers);
     }
 }
