6c63c6f1c2db492c5056a6e45b5c333d56c1586c
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-12 13:27:52 +0700

    151 The Iterator Interface

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -9,6 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
         var list = new GenericList<String>();
+        list.add("a");
+        list.add("b");
         for(var item:list)
             System.out.println(item);
 
