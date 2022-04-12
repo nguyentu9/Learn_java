3b8aad6b8645049913b6922730da5ba90d37ee7c
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-12 08:21:37 +0700

    143 Multiple Type Parameters

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -6,10 +6,6 @@ import com.example.generics.Utils;
 public class Main {
 
     public static void main(String[] args) {
-        var max = Utils.max(1, 3);
-        System.out.println(max);
-
-        var max2 = Utils.max(new User(10), new User(20));
-        System.out.println(max2);
+        Utils.print(1, "1V");
     }
 }
