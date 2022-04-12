ddbdff9de3bd928bdaf2a0c34bdce385969072f6
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-12 08:37:32 +0700

    144 Generic Classes and Inheritance

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -1,11 +1,18 @@
 package com.example;
 
+import com.example.generics.GenericList;
+import com.example.generics.Instructor;
 import com.example.generics.User;
 import com.example.generics.Utils;
 
 public class Main {
 
     public static void main(String[] args) {
-        Utils.print(1, "1V");
+        User user = new Instructor(10);
+        Utils.printUser(new Instructor(10));
+
+        var instructors = new GenericList<Instructor>();
+        var users = new GenericList<User>();
+        Utils.printUsers(users);
     }
 }
