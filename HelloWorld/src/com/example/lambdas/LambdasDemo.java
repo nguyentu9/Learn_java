d09d21d9a6861bac83be159b8fd1e4cf43b67f5d
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-13 08:19:08 +0700

    164 Lambda Expressions

--- HelloWorld/src/com/example/lambdas/LambdasDemo.java
+++ HelloWorld/src/com/example/lambdas/LambdasDemo.java
@@ -2,13 +2,23 @@ package com.example.lambdas;
 
 public class LambdasDemo {
     public static void show() {
+        // C1:
 //        greet(new ConsolePrinter());
-        greet(new Printer() {
-            @Override
-            public void print(String message) {
-                System.out.println(message);
-            }
-        });
+
+       // C2:
+//        greet(new Printer() {
+//            @Override
+//            public void print(String message) {
+//                System.out.println(message);
+//            }
+//        });
+
+        // C3
+//        greet(message -> System.out.println(message));
+
+        Printer printer = message -> System.out.println(message);
+        greet(printer);
+
     }
     public static void greet(Printer printer){
         printer.print("Hello World");
