179233870d24a9688c558b456a139b58ad99ee89
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-13 13:51:24 +0700

    172 Composing Functions

--- HelloWorld/src/com/example/lambdas/LambdasDemo.java
+++ HelloWorld/src/com/example/lambdas/LambdasDemo.java
@@ -54,17 +54,33 @@ public class LambdasDemo {
 //
 //     list.forEach(print.andThen(printUpperCase).andThen(print));
 
-        Supplier<Double> getRandom = () -> Math.random();
-        var random = getRandom.get();
-        System.out.println(random);
+//        Supplier<Double> getRandom = () -> Math.random();
+//        var random = getRandom.get();
+//        System.out.println(random);
+//
+//        Function<String, Integer> map = str -> str.length();
+//        var length = map.apply("Sky");
+//        System.out.println(length);
+//
+//        Function<Integer, Integer> mapDou = item -> item * 2;
+//        var value = mapDou.apply(4);
+//        System.out.println(value);
 
-        Function<String, Integer> map = str -> str.length();
-        var length = map.apply("Sky");
-        System.out.println(length);
 
-        Function<Integer, Integer> mapDou = item -> item * 2;
-        var value = mapDou.apply(4);
-        System.out.println(value);
+
+        // "key:value"
+        // first: "key=value"
+        // second: "{key=value}"
+        Function<String, String> replaceColon = str -> str.replace(":", "=");
+        Function<String, String> addBraces = str -> "{" + str + "}";
+
+        String result = replaceColon.andThen(addBraces).apply("key:value");
+        System.out.println(result);
+
+        String result2 = addBraces.compose(replaceColon).apply("key:value");
+        System.out.println(result2);
+
+
     }
 
     public static void greet(Printer printer) {
