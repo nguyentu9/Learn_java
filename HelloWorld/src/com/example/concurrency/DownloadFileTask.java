d0ae3c623017f9f16989511d1bab2cb3643967fc
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-16 15:48:47 +0700

    201 Race Conditions

--- HelloWorld/src/com/example/concurrency/DownloadFileTask.java
+++ HelloWorld/src/com/example/concurrency/DownloadFileTask.java
@@ -1,13 +1,21 @@
 package com.example.concurrency;
 
 public class DownloadFileTask implements Runnable{
+
+
+    private DownloadStatus status;
+
+    public DownloadFileTask(DownloadStatus status) {
+        this.status = status;
+    }
+
     @Override
     public void run() {
         System.out.println("Downloading a file: " + Thread.currentThread().getName());
 
-        for(var i = 0; i < Integer.MAX_VALUE; i++){
+        for(var i = 0; i < 10_000; i++){
             if(Thread.currentThread().isInterrupted()) break;
-            System.out.println("Downloading byte " + i);
+            status.incrementTotalBytes();
         }
 
         System.out.println("Downloading complete: " + Thread.currentThread().getName());
--- HelloWorld/src/com/example/concurrency/ThreadDemo.java
+++ HelloWorld/src/com/example/concurrency/ThreadDemo.java
@@ -1,17 +1,38 @@
 package com.example.concurrency;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadDemo {
     public static void show() {
-        Thread thread = new Thread(new DownloadFileTask());
-        thread.start();
+//        Thread thread = new Thread(new DownloadFileTask());
+//        thread.start();
+//
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        thread.interrupt();
 
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        List<Thread> threads = new ArrayList<>();
+        var status = new DownloadStatus();
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(new DownloadFileTask(status));
+            thread.start();
+            threads.add(thread);
         }
 
-        thread.interrupt();
+        for(Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println(status.getTotalBytes());
 
 
     }
