feaddba6e5b028f3e7998199264a84f5abe1af68
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-16 11:15:37 +0700

    198 Joining a Thread

--- HelloWorld/src/com/example/concurrency/ThreadDemo.java
+++ HelloWorld/src/com/example/concurrency/ThreadDemo.java
@@ -2,11 +2,15 @@ package com.example.concurrency;
 
 public class ThreadDemo {
     public static void show() {
-        System.out.println(Thread.currentThread().getName());
+        Thread thread = new Thread(new DownloadFileTask());
+        thread.start();
 
-        for(var i = 0; i < 10; i++) {
-            Thread thread = new Thread(new DownloadFileTask());
-            thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        System.out.println("File is ready to be scanned.");
     }
 }
