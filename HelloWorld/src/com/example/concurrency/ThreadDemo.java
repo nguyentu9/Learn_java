92ba50b9fc5f93580e37cafdbd11632ae169f504
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-16 11:22:12 +0700

    199 Interrupting a Thread

--- HelloWorld/src/com/example/concurrency/ThreadDemo.java
+++ HelloWorld/src/com/example/concurrency/ThreadDemo.java
@@ -6,11 +6,13 @@ public class ThreadDemo {
         thread.start();
 
         try {
-            thread.join();
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        System.out.println("File is ready to be scanned.");
+        thread.interrupt();
+
+
     }
 }
