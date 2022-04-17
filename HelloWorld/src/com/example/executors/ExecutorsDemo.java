744768691a35104c23b9dda0cac6b9ab61028ea1
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-17 15:23:31 +0700

    216 Callables and Futures

--- HelloWorld/src/com/example/executors/ExecutorsDemo.java
+++ HelloWorld/src/com/example/executors/ExecutorsDemo.java
@@ -1,14 +1,24 @@
 package com.example.executors;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 
 public class ExecutorsDemo {
    public static void show() {
        var executor = Executors.newFixedThreadPool(2);
        try {
-           executor.submit(() -> {
-               System.out.println(Thread.currentThread().getName());
+           var future = executor.submit(() -> {
+               LongTask.simulate();
+               return 1;
            });
+
+           System.out.println("Do more work");
+           try {
+               var result = future.get();
+               System.out.println(result);
+           } catch (InterruptedException | ExecutionException e) {
+               e.printStackTrace();
+           }
        } finally {
            executor.shutdown();
        }
---
+++ HelloWorld/src/com/example/executors/LongTask.java
@@ -0,0 +1,11 @@
+package com.example.executors;
+
+public class LongTask {
+    public static void simulate() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
