1c92ab8f17ec09bb324d44218709de32d2819a02
nguyentu9 <huynhthanhtoan517@gmail.com>
2022-04-17 20:12:49 +0700

    220 Implementing an Asynchronous API

--- HelloWorld/src/com/example/Main.java
+++ HelloWorld/src/com/example/Main.java
@@ -6,6 +6,7 @@ import com.example.collections.SetDemo;
 import com.example.concurrency.ThreadDemo;
 import com.example.executors.CompletableFuturesDemo;
 import com.example.executors.ExecutorsDemo;
+import com.example.executors.MailService;
 import com.example.generics.GenericList;
 import com.example.generics.Instructor;
 import com.example.generics.User;
@@ -28,6 +29,36 @@ public class Main {
 //        CreatingStreamsDemo.show();
 //        ThreadDemo.show();
 //        ExecutorsDemo.show();
-        CompletableFuturesDemo.show();
+//        CompletableFuturesDemo.show();
+
+
+
+
+        // 220 Implementing an Asynchronous API
+
+        // Lần 1
+//        var service = new MailService();
+//        service.send();
+//        System.out.println("hello world");
+
+// KQ:
+//        Mail was sent.
+//        hello world
+
+        // Lần 2
+        var service = new MailService();
+        service.sendAsync();
+        System.out.println("hello world");
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+// KQ:
+//        hello world
+//        Mail was sent.
+
     }
 }
