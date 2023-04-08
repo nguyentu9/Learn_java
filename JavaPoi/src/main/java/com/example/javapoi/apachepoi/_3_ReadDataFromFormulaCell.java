d93f32f9bc804575a973277e46e99594756f11b0
toanht.tgg <huynhthanhtoan517@gmail.com>
2023-04-08 15:05:14 +0700

    3. read data from formula cell in excel

---
+++ JavaPoi/src/main/java/com/example/javapoi/apachepoi/_3_ReadDataFromFormulaCell.java
@@ -0,0 +1,46 @@
+package com.example.javapoi.apachepoi;
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class _3_ReadDataFromFormulaCell {
+    public static void main(String[] args) throws IOException {
+        FileInputStream file = new FileInputStream(".\\readformula.xlsx");
+
+        XSSFWorkbook workbook = new XSSFWorkbook(file);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        int rows = sheet.getLastRowNum();
+        int cols = sheet.getRow(0).getLastCellNum();
+
+        for (int r = 0; r < rows; r++) {
+            XSSFRow row = sheet.getRow(r);
+            for (int c = 0; c < cols; c++) {
+                XSSFCell cell = row.getCell(c);
+                switch (cell.getCellType()) {
+                    case STRING:
+                        System.out.print(cell.getStringCellValue());
+                        break;
+                    case NUMERIC:
+                        System.out.print(cell.getNumericCellValue());
+                        break;
+                    case BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue());
+                        break;
+                    case FORMULA:
+                        System.out.print(cell.getNumericCellValue());
+                        break;
+                }
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+
+    }
+}
