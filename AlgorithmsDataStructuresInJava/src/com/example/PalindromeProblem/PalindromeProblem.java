378ff262b7ce11aadf46fe6dbec0552d9b64f29d
toanht.tgg <huynhthanhtoan517@gmail.com>
2023-04-15 23:58:59 +0700

    5. 13. Palindrome problem solution

---
+++ AlgorithmsDataStructuresInJava/src/com/example/PalindromeProblem/PalindromeProblem.java
@@ -0,0 +1,16 @@
+package com.example.PalindromeProblem;
+
+public class PalindromeProblem {
+    // it has O(N) linear running time complexity
+    public boolean solve(String s) {
+        int forward = 0;
+        int backward = s.length() - 1;
+
+        while (forward < backward) {
+            if (s.charAt(forward) != s.charAt(backward)) return false;
+            forward++;
+            backward--;
+        }
+        return true;
+    }
+}
