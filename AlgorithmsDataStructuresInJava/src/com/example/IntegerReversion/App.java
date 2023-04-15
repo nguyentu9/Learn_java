caaabaaa78a4036e93604d6108763258517bed1d
toanht.tgg <huynhthanhtoan517@gmail.com>
2023-04-16 00:47:15 +0700

    5. 14. Integer reversion problem solution

---
+++ AlgorithmsDataStructuresInJava/src/com/example/IntegerReversion/App.java
@@ -0,0 +1,9 @@
+package com.example.IntegerReversion;
+
+public class App {
+    public static void main(String[] args) {
+        IntegerReversion problem = new IntegerReversion();
+        System.out.println(problem.reverse(123456));
+
+    }
+}
